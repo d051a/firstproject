c675d36283f804378e52860ff337ad95e6b689bf
d051a <akargavin@gmail.com>
2018-10-12 16:57:41 +0300

    add Server and Technic models

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from serversapp.models import Server
 
-# Create your models here.
+class Tehnic (models.Model):
+    TECHNICTYPES = (
+        ('CMT', 'КМТ'),
+        ('SERVER', 'Сервер'),
+        ('WORKSTATION', 'Компьютер'),
+        ('CERT', 'Сертификат'),
+        ('FLASH', 'Флеш-накопитель'),
+        ('TOKEN', 'Токен'),
+    )
+
+    inventorynum1 = models.CharField(max_length=50,
+        help_text = 'Инвентарный номер#1',)
+    inventorynum2 = models.CharField(max_length=50,
+        help_text = 'Инвентарный номер#2',)
+    serialnum = models.CharField(max_length=50,
+        help_text = 'Серийный номер',)
+    tecnic = models.OneToOneField(
+        Server,
+        on_delete=models.CASCADE,)
+    technictype = models.CharField(max_length=50,
+        default = 'CMT',
+        help_text = 'Тип техники',
+        choices=TECHNICTYPES,)
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'Техника'
+        verbose_name_plural = 'Техника'
+
+    def __str__(self):
+        return str(self.id) + str(Server.name)
--- serversapp/models.py
+++ serversapp/models.py
@@ -1,3 +1,17 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+class Server (models.Model):
+    technic_id = models.AutoField(primary_key=True,)
+    ip = models.CharField(max_length=50,
+            help_text = 'Ip-адрес',)
+    name = models.CharField(max_length=50,
+                help_text = 'Имя',)
+    class Meta: #настройка вывода данных
+        ordering = ['name'] #сортировка записей по полю. "-" - сортировка с конца
+        verbose_name = 'Сервер' #вывод названия модели в единственном числе
+        verbose_name_plural = 'Серверы' #вывод названия модели в единственном числе
+
+    def __str__(self):
+        return self.name
