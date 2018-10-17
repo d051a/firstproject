3479243531905f74b2680d58048cde6d7d9ed3e5
d051a <akargavin@gmail.com>
2018-10-17 18:00:39 +0300

    add Ticket models

--- ticketsapp/admin.py
+++ ticketsapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import MainProblem, SubProblem, Ticket
 
 # Register your models here.
+admin.site.register(MainProblem)
+admin.site.register(SubProblem)
+admin.site.register(Ticket)
--- ticketsapp/models.py
+++ ticketsapp/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+class Ticket(models.Model):
+    STATUSES = (
+        ('OPEN', 'Открыта'),
+        ('INWORK', 'В работе'),
+        ('CLOSED', 'Закрыта'),)
+    PRIORITY = (
+        ('LOW', 'Низкий'),
+        ('NORMAL', 'Обычный'),
+        ('HIGH', 'Высокий'),)
+    status = models.CharField('Статус заявки', max_length=50,
+        blank=False,
+        choices=STATUSES,)
+    priority = models.CharField('Приоритет', max_length=50,
+            blank=False,
+            choices=PRIORITY,)
+    description = models.CharField('Описание проблемы', max_length=300)
+    timestarted = models.DateTimeField('Дата и время подачи заявки', auto_now_add=True)
+    timeclosed = models.DateTimeField('Дата и время закрытия заявки',)
+    mainproblem = models.ForeignKey(
+        'MainProblem', default=None)
+    subproblem = models.ForeignKey(
+            'SubProblem', default=None)
+
+
+class MainProblem(models.Model):
+    mainproblemname = models.CharField(max_length=30, null=True)
+
+    def __str__(self):
+        return self.mainproblemname
+
+
+class SubProblem(models.Model):
+    mainproblem = models.ForeignKey(MainProblem, on_delete=models.CASCADE, default=None)
+    subproblemname = models.CharField(max_length=30, null=True)
+
+    def __str__(self):
+        return self.subproblemname
