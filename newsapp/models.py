842c5781ed8d017ff9d2c1925967fc3aef46fda6
d051a <akargavin@gmail.com>
2019-01-11 16:25:18 +0300

    add holidays view page

--- newsapp/models.py
+++ newsapp/models.py
@@ -4,13 +4,13 @@ from django.db import models
 class News (models.Model):
     NEWSTYPE = (('Основные новости', 'MAIN'),
                 ('Новости портала', 'PORTAL'))
-    title = models.CharField('Заголовок', max_length=50, null=False)
+    title = models.CharField('Заголовок', max_length=200, null=False)
     content = models.TextField('Контент', max_length=10000, null=False)
     type = models.CharField('Тип новости', max_length=50, null=False, choices=NEWSTYPE)
     datetime = models.DateTimeField('Дата и время создания', auto_now_add=True)
 
     class Meta:
-        ordering = ['title']
+        ordering = ['-datetime']
         verbose_name = 'Новость'
         verbose_name_plural = 'Новости'
 
--- newsapp/views.py
+++ newsapp/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView
 from .models import News
 from holidaysapp.models import Holiday
 from employeesapp.models import Employee
@@ -24,5 +24,25 @@ class NewsList(ContextPageMixin, ListView):
         context['birthdays_list'] = birthdays_list_gte[:5]
         return context
 
+
 '''class NewsAdd (ContextPageMixin, CreateView):
     template_name'''
+
+
+class NewsPageView(ContextPageMixin, UpdateView):
+    template_name = 'newsapp/edit_news.html'
+    model = News
+    context_object_name = 'news'
+    pagename = 'Новости Департамента'
+    fields = 'title', 'content'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        today = datetime.now().date()
+        holiday_list_gte = Holiday.objects.filter(date__day__gte=today.day,
+            date__month__gte=today.month)
+        birthdays_list_gte = Employee.objects.filter(birthdate__day__gte=today.day,
+            birthdate__month__gte=today.month)
+        context['holidays_list'] = holiday_list_gte[:5]
+        context['birthdays_list'] = birthdays_list_gte[:5]
+        return context
